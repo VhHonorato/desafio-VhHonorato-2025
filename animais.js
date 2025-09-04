afae0fbbd3626cf7ec0ec7b176610bc8c3573496
vhhonorato <vhhonorato@hotmail.com>
2025-09-04 19:25:36 -0300

    feat(dados): Centraliza as informações dos animais em um arquivo dedicado
    Cria um arquivo 'animais.js' para compilar as infomações sobre os animais do abrigo, como sua espécie e a lista de brinquedos preferidos.
    
    Essa alternativa facilita a manutençào e escalabilidade do sistema.

---
+++ animais.js
@@ -0,0 +1,32 @@
+const animais = {
+  Rex: {
+    especie: 'cão',
+    brinquedos: ['RATO', 'BOLA'],
+  },
+  Mimi: {
+    especie: 'gato',
+    brinquedos: ['BOLA', 'LASER'],
+  },
+  Fofo: {
+    especie: 'gato',
+    brinquedos: ['BOLA', 'RATO', 'LASER'],
+  },
+  Zero: {
+    especie: 'gato',
+    brinquedos: ['RATO', 'BOLA'],
+  },
+  Bola: {
+    especie: 'cão',
+    brinquedos: ['CAIXA', 'NOVELO'],
+  },
+  Bebe: {
+    especie: 'cão',
+    brinquedos: ['LASER', 'RATO', 'BOLA'],
+  },
+  Loco: {
+    especie: 'jabuti',
+    brinquedos: ['SKATE', 'RATO'],
+  },
+};
+
+export { animais };
---
+++ src/abrigo-animais.js
@@ -0,0 +1,12 @@
+class AbrigoAnimais {
+
+  encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+    try {
+      
+      if()
+    } catch (error) {
+      
+    }
+}
+
+export { AbrigoAnimais as AbrigoAnimais };
