23997807cb85b981058d395cfc24be288b436b78
vhhonorato <vhhonorato@hotmail.com>
2025-09-04 21:01:46 -0300

    feat(validacao): Criada validação para animal inválido
    Implementada validação para os casos de inserção dos dados de animais inválidos.

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,12 +1,25 @@
-class AbrigoAnimais {
+import { animais } from "../animais.js";
+
+export class AbrigoAnimais {
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
     try {
+      const listaAnimais = ordemAnimais.split(',');
+      const animalInvalido = listaAnimais.find(animal => !animais[animal]);
       
-      if()
+      if(animalInvalido) {
+        return { erro: 'Animal inválido' };
+      }
     } catch (error) {
-      
+      return { erro: error.message}
     }
 }
+}
 
-export { AbrigoAnimais as AbrigoAnimais };
+
+
+//export { AbrigoAnimais as AbrigoAnimais };
+
+const resultadoTeste = new AbrigoAnimais().encontraPessoas('RATO,BOLA', 'RATO,NOVELO', 'Lulu');
+console.log('Resultado do teste rápido:', resultadoTeste);
+
