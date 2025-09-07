236e2bbdabceaf70d8a0632542134ef8f8180203
vhhonorato <vhhonorato@hotmail.com>
2025-09-06 21:06:04 -0300

    feat(validacao): Implementada validações paras regras 1, 2, 3, e 4.
    - Validada regra 1 - onde a pessoa precisa oferecer os brinquedos favoritos na ordem correta para acontecer a adoção.
    - Validada regra 2 - onde os brinquedos favoritos oferecidos, mesmo que intercalados com outros brinquedos não favoritos, estando na ordem correta, satisfazem a condição para adoção.
    - Validada regra 3 - onde se já houver algum animal adotado com, pelo menos um dos brinquedos favoritos do gato em questão, os dois não poderão ser adotados pela mesma pessoa. Já que gatos não dividem seus brinquedos e esse brinquedo em comum não poderia satisfazer a condição de adoção dos dois animais. Portanto, prevalece o animal que teve a condição de adoção satisfeita primeiro.
    - Validada regra 4 - onde se ambos os brinquedos oferecidos pelas duas pessoas satisfazem um animal, ele não será adotado por nenhum dos dois e retornará ao abrigo. Na mesma validação da regra 4 tmabém é atendida a condição de caso nenhum dos brinquedos oferecidos pelas duas pessoas satisfazem o animal e ele retornará para o abrigo da mesma forma.

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,4 +1,5 @@
 import { animais } from "../animais.js";
+import { arraysIguaisENaOrdem } from "../utilitarios.js";
 
 
 export class AbrigoAnimais {
@@ -29,7 +30,85 @@ export class AbrigoAnimais {
 
       }
       
-    } catch (error) {
+      // Lógica Adoção
+      const resultadoFinal = [];
+      const animaisAdotadosPessoa1 = [];
+      const animaisAdotadosPessoa2 = [];
+      const nomeGatos1 = [];
+      const nomeGatos2 = [];
+
+      for(const nomeAnimal of listaAnimais){
+        const brinquedosFavoritos = animais[nomeAnimal].brinquedos;
+        const especieAnimal = animais[nomeAnimal].especie;
+
+        const pessoaApta1 = arraysIguaisENaOrdem(brinquedosFavoritos, listaBrinquedosPessoa1);
+        const pessoaApta2 = arraysIguaisENaOrdem(brinquedosFavoritos, listaBrinquedosPessoa2);
+
+        //Regra 4 - No caso de as duas pessoas serem aptas a adoção. E atende tambem a condição de as duas !aptas.
+        if(pessoaApta1 === pessoaApta2){
+           resultadoFinal.push(`${nomeAnimal} - abrigo`)
+         
+           continue;
+        }
+        if(pessoaApta1){
+          
+          if(animais[nomeAnimal].especie === 'gato'){
+            nomeGatos1.push(nomeAnimal);
+            //Regra 3 - Se já houver um animal adotado com pelo menos um dos brinquedos favoritos iguais ao do gato em questão, os dois não poderão ser adotados pela mesma pessoa e prevalce quem foi escolhido primeiro.
+            for(let i=0; i<animaisAdotadosPessoa1.length; i++){
+              const brinquedoIgual = animais[animaisAdotadosPessoa1[i].nome].brinquedos.find(brinquedo => animais[nomeGatos1[0]].brinquedos.includes(brinquedo));
+              if(animaisAdotadosPessoa1.length >= 1){ 
+                if(brinquedoIgual){
+                resultadoFinal.push(`${nomeAnimal} - abrigo`);
+                
+                } 
+              continue;
+              } 
+            
+            }
+            if(animaisAdotadosPessoa1.length < 1){
+            resultadoFinal.push(`${nomeAnimal} - pessoa 1`);
+            animaisAdotadosPessoa1.push({nome: nomeAnimal, especie: animais[nomeAnimal].especie});
+            }
+
+          }else if(animais[nomeAnimal].especie !== 'gato'){
+            resultadoFinal.push(`${nomeAnimal} - pessoa 1`);
+            animaisAdotadosPessoa1.push({nome: nomeAnimal, especie: animais[nomeAnimal].especie});
+            }          
+      
+        }
+
+        if(pessoaApta2){
+          
+          if(animais[nomeAnimal].especie === 'gato'){ 
+            nomeGatos2.push(nomeAnimal);
+            // Atendendo a regra 3, se já houver um animal adotado com pelo menos um dos brinquedos favoritos iguais ao do gato em questão, os dois não poderão ser adotados pela mesma pessoa e prevalce quem foi escolhido primeiro.
+            
+              if(animaisAdotadosPessoa2.length >= 1){
+            for(let i=0; i<animaisAdotadosPessoa2.length; i++){
+              const brinquedoIgual = animais[animaisAdotadosPessoa2[i].nome].brinquedos.find(brinquedo => animais[nomeGatos2[0]].brinquedos.includes(brinquedo));
+              if(brinquedoIgual){
+                resultadoFinal.push(`${nomeAnimal} - abrigo`);
+              }
+              continue;
+            }
+            
+          }
+            if(animaisAdotadosPessoa2.length < 1){  
+              resultadoFinal.push(`${nomeAnimal} - pessoa 2`);
+              animaisAdotadosPessoa2.push({nome: nomeAnimal, especie: animais[nomeAnimal].especie});
+            }
+            
+         
+          }else if(animais[nomeAnimal].especie !== 'gato'){
+            resultadoFinal.push(`${nomeAnimal} - pessoa 2`);
+          } 
+        }
+      continue;  
+    
+      }
+        return { lista: resultadoFinal.sort() };
+    }catch (error) {
       return { erro: 'Ocorreu um erro inesperado no processamento.' };
     }
   }
@@ -40,6 +119,8 @@ export class AbrigoAnimais {
 
 // Linha para teste rápido durante o desenvolvimento
 
-const resultadoTeste = new AbrigoAnimais().encontraPessoas('RATO,BOLA', 'RATO,PENA', 'Rex,Fofo');
+const resultadoTeste = new AbrigoAnimais().encontraPessoas('BOLA,LASER',
+      'BOLA,NOVELO,RATO,LASER', 'Mimi,Fofo,Rex,Bola');
 console.log('Resultado do teste rápido:', resultadoTeste);
+
 
