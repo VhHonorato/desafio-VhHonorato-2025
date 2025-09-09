92a8804816316ca37b33f48c8dc6cbea9f2544a2
vhhonorato <vhhonorato@hotmail.com>
2025-09-09 15:24:29 -0300

    fix(validacao): Corrigido bug encontrado pelos testes na lógica das validações.
    Após a adição de novos cenários de testes, foram reportados alguns bugs no código, em relação as validações de gatos e de Loco
    - Refatorada função 'podeAdotarGato' para:
        - Verificar se a pessoa está apta;
        - Verificação para no caso do  animal candidato a adocão não ser um gato. Verificando também se ja existe animais adotados e se existe algum gato entre eles.
    - Corrigindo as condições dos if/eles para que atendessem as regras dos testes, levando em consideração a refatoração das funções utilitarias.

--- utilitarios.js
+++ utilitarios.js
@@ -11,12 +11,10 @@ function brinquedosIguaisENaOrdem(brinquedosAnimal, brinquedosPessoa) {
     for (let indicePessoa = 0; indicePessoa < brinquedosPessoa.length; indicePessoa++){
         if(brinquedosAnimal[indiceAnimal] === brinquedosPessoa[indicePessoa]){
             indiceAnimal++;
-        
         }
         if(indiceAnimal === brinquedosAnimal.length){
             return true;
         }
-    
     }
     return false;
 }
@@ -41,17 +39,46 @@ function adotouMaisDeTres(animaisAdotadosPessoa){
  * @param {Array<Object>} animaisAdotadosPessoa A lista de animais já adotados.
  * @returns {boolean} 'true' se o gato pode ser adotado, `false` caso contrário.
  */
-function podeAdotarGato(nomeAnimal, animaisAdotadosPessoa){
-    const brinquedoGatoAtual = new Set(animais[nomeAnimal].brinquedos);
-    for(const animal of animaisAdotadosPessoa){
-        const brinquedoAnimalAdotado = new Set(animais[animal.nome].brinquedos);
-        const compartilhaBrinquedo = brinquedoAnimalAdotado.some(brinquedo =>brinquedoGatoAtual.has(brinquedo));
-        if(compartilhaBrinquedo){
-            return false;
-        }
-    
+function podeAdotarGato(nomeAnimal, animaisAdotadosPessoa, pessoaApta){
+    if(!pessoaApta){
+        return false;
     }
-    return true;
+    if(animais[nomeAnimal].especie === 'gato'){
+        const brinquedoGatoAtual = new Set(animais[nomeAnimal].brinquedos);
+        if(animaisAdotadosPessoa.length < 1){
+            return true;
+        }
+        for(const animal of animaisAdotadosPessoa){
+            const brinquedoAnimalAdotado = animais[animal.nome].brinquedos;
+            const compartilhaBrinquedo = brinquedoAnimalAdotado.some(brinquedo =>brinquedoGatoAtual.has(brinquedo));
+            if(compartilhaBrinquedo){
+                return false;
+            }else{
+                return true;
+            }
+        }
+        
+    } else if(animais[nomeAnimal].especie !== 'gato'){
+        const brinquedoAnimalAtual = new Set(animais[nomeAnimal].brinquedos);
+        if(animaisAdotadosPessoa.length < 1){
+            return true;
+        }
+        for(const animal of animaisAdotadosPessoa){
+            if(animal.especie === 'gato'){    
+                const brinquedoGatoAdotado = animais[animal.nome].brinquedos;
+                const compartilhaBrinquedo = brinquedoGatoAdotado.some(brinquedo =>brinquedoAnimalAtual.has(brinquedo));
+                if(compartilhaBrinquedo){
+                return false;
+                } else {
+                     return true;
+                }
+                  
+            }
+          
+        }
+    }
+    
+   
 }
 
 /** 
@@ -63,29 +90,52 @@ function podeAdotarGato(nomeAnimal, animaisAdotadosPessoa){
  * @returns {boolean} 'true' se o array com os brinquedos da pessoa estiverem com conteúdo e ordem corretos.
  */
 function podeAdotarLoco(nomeAnimal,animaisAdotadosPessoa, brinquedosAnimal, brinquedosPessoa){
-    let indiceAnimal = 0;
-    if(nomeAnimal === 'Loco'){      
-        for(const brinquedo of brinquedosPessoa){
-            const brinquedoValido = brinquedosAnimal.includes(brinquedo);
-            if(brinquedoValido){
-                indiceAnimal++;
-            }
-        }
-        if(indiceAnimal !== brinquedosAnimal.length){
-        return false;
-        } else if (animaisAdotadosPessoa.length >= 1){
-            return true;
-
-        }
-     
-    } else {
+    const brinquedoPessoa = new Set(brinquedosPessoa);
+    const temTodosOsBrinquedos = brinquedosAnimal.every(b => brinquedoPessoa.has(b));
+    if (nomeAnimal !== 'Loco') {
         return false;
     }
-}            
+
+    // Regra 6.2: Loco precisa de companhia.
+    if (animaisAdotadosPessoa.length < 1) {
+        return false;
+    } else if (temTodosOsBrinquedos){  //  A pessoa deve ter todos os brinquedos do Loco, sem se importar com a ordem.
+        return true;
+    }
+
+   
+    
+
+}
+
+/** 
+ * Verifica se Loco pode ser adotado, checando se existe os brinquedos preferidos, independente da ordem, e se ele não vai ser adotado sozinho.
+ * @param {string} nomeAnimal O nome do animal a ser verificado.
+ * @param {Array<Object>} animaisAdotadosPessoa A lista de animais já adotados.
+ * @returns {boolean} 'true' se o array com os brinquedos da pessoa estiverem com conteúdo e ordem corretos.
+ */
+
+function podeAdotarComGato (nomeAnimal, animaisAdotadosPessoa){
+    const brinquedosAnimalAtual = new Set(animais[nomeAnimal].brinquedos);
+    const temGatoAdotado = animaisAdotadosPessoa.filter(animal => animal.especie === 'gato');
+    if(temGatoAdotado){
+        const compartilhaBrinquedo = animais[temGatoAdotado.nome].brinquedos.some(brinquedo => brinquedosAnimalAtual.has(brinquedo));
+        if(compartilhaBrinquedo){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
 export { 
     brinquedosIguaisENaOrdem,
     adotouMaisDeTres,
     podeAdotarGato,
-    podeAdotarLoco  
+    podeAdotarLoco,
+    podeAdotarComGato  
  
 }
